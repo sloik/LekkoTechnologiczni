d1eba76f4400fcda0af46d55d73a62b30cfc5eb2
Lukasz Stocki <stocki.lukasz@gmail.com>
2019-10-02 21:20:40 +0200

    WiP: refactor pierwsza faza

---
+++ Kik/Kik/View/KikBaseViewController.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+class KikBaseViewController: UIViewController {
+
+    @IBOutlet var buttons: [UIButton]!
+    var viewModel = KikViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupGame()
+        addAccesibilityIdentifiers()
+    }
+
+    func setupGame() {
+        viewModel.resetGame()
+        refreshButtons()
+
+        viewModel.delegate = self
+    }
+
+
+    @IBAction func didTapButton(_ sender: UIButton) {
+        viewModel.didTapElement(sender.tag)
+        refreshButtons()
+    }
+
+    func refreshButtons() {
+        buttons
+            .forEach { button in
+                button.setTitle(viewModel.title(for: button.tag), for: .normal) }
+    }
+}
+
+extension KikBaseViewController: KikViewModelDelegate {
+    func showWinner(grid: String) {
+        let ok = UIAlertAction(title: "New Game",
+        style: UIAlertAction.Style.default) { _ in
+            self.setupGame()
+        }
+
+        let alert = UIAlertController(title: "🤩 Game won by" ,
+                          message: grid,
+                          preferredStyle: .alert)
+
+        alert.addAction(ok)
+
+        present(alert, animated: true, completion: nil)
+    }
+
+    func showTie(grid: String) {
+        let ok = UIAlertAction(title: "New Game",
+        style: UIAlertAction.Style.default) { _ in
+            self.setupGame()
+        }
+
+        let alert = UIAlertController(title: "🤔 No one won!" ,
+                          message: grid,
+                          preferredStyle: .alert)
+
+        alert.addAction(ok)
+
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension KikBaseViewController {
+    
+    func addAccesibilityIdentifiers() {
+        for button in buttons {
+            button.isAccessibilityElement = true
+            button.accessibilityIdentifier = String(button.tag)
+        }
+    }
+}
