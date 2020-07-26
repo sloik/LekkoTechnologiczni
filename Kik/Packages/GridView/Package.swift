4f61e1e006456b650920c1c8a391ab568366c055
Lukasz Stocki <stocki.lukasz@gmail.com>
2020-07-26 15:08:28 +0200

    Moves to package.

---
+++ Kik/Packages/GridView/Package.swift
@@ -0,0 +1,50 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GridView",
+    platforms: [
+//         .macOS(.v10_10),
+        .iOS(.v11),
+        .tvOS(.v11),
+        .watchOS(.v3)
+    ],
+
+    products: [
+        .library(
+            name: "GridView",
+            type: .static,
+            targets: ["GridView"]),
+    ],
+
+    dependencies: [
+        .package(
+            name: "Prelude",
+            url:  "https://github.com/pointfreeco/swift-prelude.git",
+            .branch("main")
+        ),
+
+        .package(
+            name: "SnapshotTesting",
+            url: "https://github.com/pointfreeco/swift-snapshot-testing.git",
+            from: "1.8.1"
+        ),
+    ],
+
+    targets: [
+        .target(
+            name: "GridView",
+            dependencies: [
+                .product(name: "Either", package: "Prelude"),
+            ]
+        ),
+        .testTarget(
+            name: "GridViewTests",
+            dependencies: [
+                "GridView",
+                "SnapshotTesting",
+            ]),
+    ]
+)
