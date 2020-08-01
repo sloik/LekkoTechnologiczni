23f96b7d27221c296adbaf8995098ece9cbe5d60
Lukasz Stocki <stocki.lukasz@gmail.com>
2020-08-01 16:16:38 +0200

    WiP: Adds more methods

--- Kik/Packages/KikPackage/Sources/KikImplementation/KikImplementation.swift
+++ Kik/Packages/KikPackage/Sources/KikImplementation/KikImplementation.swift
@@ -1,7 +1,11 @@
+// https://gist.github.com/swlaschin/7a5233a91912e66ac1e4
+// https://fsharpforfunandprofit.com/posts/enterprise-tic-tac-toe/
+// ENTERPRISE TIC-TAC-TOE - Scott Wlaschin https://vimeo.com/131658142
 
 import Foundation
 import KikDomain
 import Prelude
+import OptionalAPI
 
 // private implementation of game state
 struct GameState {
@@ -20,8 +24,8 @@ let allVertPositions: [VertPosition] = VertPosition.allCases
 /// A type to store the list of cell positions in a line
 //    type Line = Line of CellPosition list
 
-struct Line {
-    let cells: [CellPosition]
+struct Line: Equatable {
+    let cellsPositions: [CellPosition]
 }
 
 
@@ -35,7 +39,7 @@ let linesToCheck: [Line] = {
             .allCases               // left     ...  hCenter    ...  right
             .map { h in (h, v) }    // (left, v)... (hCenter, v)... (right, v)
             .map ( CellPosition.init(hp: vp:) ) // CP{left, v}... CP{hCenter, v)
-            |> Line.init(cells:)
+            |> Line.init(cellsPositions:)
     }
 
     let horizontalLines: [Line] = VertPosition.allCases.map(makeHLine)
@@ -47,18 +51,18 @@ let linesToCheck: [Line] = {
             .allCases
             .map { v in (h, v) }
             .map( CellPosition.init(hp:vp:) )
-            |> Line.init(cells:)
+            |> Line.init(cellsPositions:)
     }
 
     let verticalLines: [Line] = HorizPosition.allCases.map(makeVLine)
 
     let diagonalLine1: Line = [(HorizPosition.left, VertPosition.top), (.hCenter, .vCenter), (.right, .bottom)]
         .map( CellPosition.init(hp:vp:) )
-        |> Line.init(cells:)
+        |> Line.init(cellsPositions:)
 
     let diagonalLine2: Line = [(.left, .bottom), (.hCenter, .vCenter), (.left, .top)]
         .map( CellPosition.init(hp:vp:) )
-        |> Line.init(cells:)
+        |> Line.init(cellsPositions:)
 
     return horizontalLines + horizontalLines + [diagonalLine2]
 }()
@@ -83,16 +87,86 @@ let getDisplayInfo: (GameState) -> DisplayInfo =
 //        gameState.cells
 //        |> List.find (fun cell -> cell.pos = posToFind)
 
-let getCell = { (gameState: GameState, posToFind: CellPosition) -> Cell in
-    gameState
-        .cells
-        .first { (cell: Cell) -> Bool in
-            cell.pos == posToFind
-        }!
-}
+let getCell: (GameState) -> (CellPosition) -> Cell =
+    { (gameState: GameState) in
+        { (posToFind: CellPosition) -> Cell in
+
+            gameState
+                .cells
+                .first { (cell: Cell) -> Bool in
+                    cell.pos == posToFind
+                }!
+        }
+    }
 
 
 /// update a particular cell in the GameState
    /// and return a new GameState
 //   let private updateCell newCell gameState =
-#error("Start here...")
+
+
+/// This function given current game state and a cell will return a new game state with
+/// the cell "inserted" in the correct place.
+private let updateCell = {  (newCell: Cell , gameState: GameState ) -> GameState in
+    gameState
+        .cells
+        .map { (cell: Cell) -> Cell in
+            cell.pos == newCell.pos // if the current cell is in place of the new one
+                ? newCell           // return the new one
+                : cell              // return original one
+        }
+        |> GameState.init(cells:)
+}
+
+
+/// Return true if the game was won by the specified player
+//   let private isGameWonBy player gameState =
+
+private let isGameWonBy = { (player: Player, gameState: GameState) -> Bool in
+
+    func cellWasPlayed(by playerToCheck: Player) -> (Cell) -> Bool {
+        { (cell: Cell) -> Bool in
+
+            switch cell.state {
+            case .played(let player):
+                return playerToCheck == player
+
+            case .empty:
+                return false
+            }
+        }
+    }
+
+    func lineIsAllSame(_ player: Player) -> (Line) -> Bool {
+        { (line: Line) in
+            line
+                .cellsPositions
+                // getCell(gameState) return a function "waiting" for cell position.
+                .map( getCell(gameState) )
+                // Here we have the cells from that line as they are currently played.
+                .allSatisfy( cellWasPlayed(by: player) )
+        }
+    }
+
+    return
+        linesToCheck
+            .first(where: lineIsAllSame(player) )
+            .isSome
+}
+
+
+
+/// Return true if all cells have been played
+//    let private isGameTied gameState =
+
+let isGameTied = { (gameState: GameState) -> Bool in
+
+    let cellWasPlayed = { (cell: Cell) -> Bool in
+        switch cell.state {
+        case .played: return true
+        case .empty: return false
+        }
+    }
+
+    return gameState.cells.allSatisfy( cellWasPlayed )
+}
